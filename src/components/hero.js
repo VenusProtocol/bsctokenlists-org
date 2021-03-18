f229fbea57792b6c5653d365d4cc7f45dc7cecb6
gryincent <68727455+gryincent@users.noreply.github.com>
2021-03-18 20:32:19 +1000

    update title

--- src/components/hero.js
+++ src/components/hero.js
@@ -78,7 +78,7 @@ export default function Header() {
         Project
       </span>
 
-      <p className="title">An Binance Smart Chain token list standard.</p>
+      <p className="title">A Binance Smart Chain token list standard.</p>
 
       <p style={{ fontSize: '20px', lineHeight: '150%' }} className="description" id="why-lists">
         Token Lists is a community-led initiative to improve discoverability, reputation and trust in BEP20 token lists
